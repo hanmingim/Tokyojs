19b0201a08cbbc85c3b64bd38cdd0319514f2865
hanmingim <rlagksalsq3@gmail.com>
2023-05-04 12:39:30 +0900

    Changed

--- js/todo.js
+++ js/todo.js
@@ -21,7 +21,7 @@ function paintToDo(newTodo) {
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerHTML = newTodo.text;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
